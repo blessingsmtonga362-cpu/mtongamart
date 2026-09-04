455d49df6f2916e99994be04e011725bf29b9a0f
Blessings Mtonga <bsc-45-21@unima.ac.mw>
2026-09-04 11:28:01 +0200

    working on the cart dammy screen

--- lib/cart.dart
+++ lib/cart.dart
@@ -4,11 +4,93 @@ class Cart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
-      children: [
-        Text("Cart"),
-        Text("data")]
-    );
+    return  SingleChildScrollView(
+        child: Padding(
+        padding: EdgeInsetsGeometry.all(10),
+      child:Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+                height: 300,
+                width: double.infinity,
+
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                 border: BoxBorder.all(
+                   color: Colors.grey,
+                   width: 3
+                 ),
+                color: Colors.white54,
+                ),
+
+                  child:Column(
+                    children: [
+                      Text("Order Summary",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.black),),
+                      SizedBox(height: 10,),
+
+                    ]
+
+                  )
+
+            ),
+            SizedBox(height: 10,),
+
+            ////////////////
+            Container(
+                height: 200,
+                width: double.infinity,
+
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  border: BoxBorder.all(
+                      color: Colors.grey,
+                      width: 3
+                  ),
+                  color: Colors.white54,
+                ),
+
+                child:Column(
+                    children: [
+                      Text("Delivery Mode",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.black),),
+                      SizedBox(height: 10,),
+
+                    ]
+
+                )
+
+            ),
+            SizedBox(height: 10,),
+            ///////
+            Container(
+                height: 200,
+                width: double.infinity,
+
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  border: BoxBorder.all(
+                      color: Colors.grey,
+                      width: 3
+                  ),
+                  color: Colors.white54,
+                ),
+
+                child:Column(
+                    children: [
+                      Text("Mode of Payment",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.black),),
+                      SizedBox(height: 10,),
+
+                    ]
+
+                )
+
+            )
+          ],
+        )
+        )));
+
+
+
   }
 }
 
